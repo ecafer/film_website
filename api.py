14944e661fc24f499c197233920f516f1df78adb
ecafer <deepsearch34@gmail.com>
2023-11-14 10:20:07 +0100

    movieList div and its logic removed. Data from form succesfully taken to the server

--- api.py
+++ api.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/enter_movie')
+@app.route('/movie_entry', methods=['GET', 'POST'])
 def enter_movie():
-    return render_template('homepage.html')
+    if request.method == "GET":
+        return render_template('homepage.html')
+    elif request.method == "POST":
+        print(request.form)
+        title = request.form.get('title')
+        print(title)
+        return render_template('homepage.html')
+    else:
+        print('UNKNOWN HTTP REQUEST')

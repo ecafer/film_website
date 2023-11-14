bc54c63d6edbbb3de7e0f68fda0ccec2045f1985
ecafer <deepsearch34@gmail.com>
2023-11-14 10:26:45 +0100

    simplified server side logic

--- api.py
+++ api.py
@@ -6,12 +6,17 @@ app = Flask(__name__)
 
 @app.route('/movie_entry', methods=['GET', 'POST'])
 def enter_movie():
-    if request.method == "GET":
-        return render_template('homepage.html')
-    elif request.method == "POST":
-        print(request.form)
-        title = request.form.get('title')
-        print(title)
-        return render_template('homepage.html')
-    else:
-        print('UNKNOWN HTTP REQUEST')
+
+    if request.method == "POST":
+        try:
+            title = request.form.get('title')
+            director = request.form.get('director')
+            release_year = request.form.get('releaseYear')
+            country = request.form.get('country')
+            myRating = request.form.get('myRating')
+            short = request.form.get('short')
+            print(title, director, release_year, country, myRating, short)
+        except KeyError:
+            print('Make sure the field exists.')
+    return render_template('homepage.html')
+

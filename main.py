54bf87a598d9398f96b4ea236de50ef17812ca49
ecafer <deepsearch34@gmail.com>
2023-11-14 17:24:18 +0100

    first very basic database command finished

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-import psycopg
+from psycopg import connect, OperationalError
+from sys import exit
 
-connection_object = psycopg.connect(host='localhost',
-                                    dbname="postgres",
-                                    user="postgres",
-                                    password="1234",
-                                    port=5432)
+# Try to connect with credentials to the database
+try:
+    connection_object = connect(host='localhost',
+                                dbname="postgres",
+                                user="postgres",
+                                password="1234",
+                                port=5432)
+except OperationalError:
+    exit('ERROR: Unable to connect to the database')
 
+# Create a cursor to execute PostgreSQ command in a database session
 cursor = connection_object.cursor()
 
-# TBD: Do some database operations
+# TBD: Do some database operations (in SQL)
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS film (
+        titel TEXT,
+        director TEXT,
+        releaseYear INT,
+        country TEXT,
+        myRating REAL,
+        short BOOLEAN
+    );
+""")
 
-
+# Commit the pending transactions to the database
 connection_object.commit()
 cursor.close()
-connection_object.clos()
+connection_object.close()
